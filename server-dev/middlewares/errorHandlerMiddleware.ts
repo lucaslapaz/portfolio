47c4ada655f1d532edba96f367b846bbeef3cffd
Lucas Lapaz <jorgeescolaceal@gmail.com>
2025-07-09 10:46:56 -0300

    fix(errorHandler): fix switch

--- server-dev/middlewares/errorHandlerMiddleware.ts
+++ server-dev/middlewares/errorHandlerMiddleware.ts
@@ -14,11 +14,15 @@ export default function errorHandlerMiddleware(error: any, request: Request, res
     switch(statusCode){
         case 500: // erro interno do servidor
             response.render('error');
+            break;
         case 401: // unauthorized
             response.render('unauthorized');
+            break;
         case 404: // not found
             response.render('404');
+            break;
         default:
             response.render('error');
+            break;
     }
 }
