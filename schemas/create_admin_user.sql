7759c5c6a4456c836a05cc8a7160562d05ebe203
Lucas Lapaz <jorgeescolaceal@gmail.com>
2025-07-30 12:03:46 -0300

    feature(home): add delphi icon

--- schemas/create_admin_user.sql
+++
@@ -1,4 +0,0 @@
-USE blog_db;
-
--- password: 123admin123
-INSERT INTO users (username, name, email, password, permission) VALUES ('admin', 'Admin', 'contact@example.com', '$2b$12$6mxsKPpN71LxTBk2cMr3TuY38mYkpOFIqMYYaNew23YDHhWBXzt4e', 10);
